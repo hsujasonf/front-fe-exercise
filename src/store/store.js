fcb03e9b52924801cff7171a6fb91edf9c91c938
Jason Hsu <hsujasonf@gmail.com>
2024-05-29 20:00:34 -0700

    finishing project, added unit tests

--- src/store/store.js
+++ src/store/store.js
@@ -1,47 +1,207 @@
 export class Store {
-  /**
-   * Returns an array of conversation objects in reverse chronological order.
-   *
-   * @returns {readonly {
-   *   id: string,
-   *   assignedUser: string | null,
-   *   subject: string,
-   *   blurb: string,
-   *   messageCount: number,
-   *   lastUpdatedTimestamp: number
-   * }[]}
-   */
-  getConversations() {
-    // TODO
-    return [
-      {
-        id: '1234abcd',
-        assignedUser: 'Laurent.Perrin',
-        subject: 'Re: Lorem ipsum',
-        blurb: 'TODO',
-        messageCount: 2,
-        lastUpdatedTimestamp: 1650949681692,
-      },
-    ];
+  constructor() {
+    this.conversations = {};
+    this.processedEvents = new Set();
+    this.eventTypes = {
+      MESSAGE_RECEIVED: 'messageReceived',
+      ASSIGNED: 'assigned',
+      UNASSIGNED: 'unassigned',
+      TYPING_STARTED: 'typingStarted',
+      TYPING_STOPPED: 'typingStopped',
+    };
   }
 
-  /**
-   * Handles an event that updates the state of a conversation.
-   *
-   * @param {{
-   *   type: string,
-   *   data: {
-   *     timestamp: number,
-   *     conversationId: string,
-   *     user?: string,
-   *     subject?: string,
-   *     body?: string
-   *   }
-   * }} event
-   * @returns {void}
-   */
+  getConversations() {
+    try {
+      // filter out conversations with John_Doe as the assigned_user
+      const conversationsArray = [];
+      for (const [id, data] of Object.entries(this.conversations)) {
+        if (data.assignedUser !== 'John_Doe') {
+          // store typingUsers and mostRecentMessage but do not display in the conversations array.
+          const { typingUsers, mostRecentMessage, ...rest } = data;
+
+          conversationsArray.push({
+            id,
+            ...rest,
+          });
+        }
+      }
+
+      conversationsArray.sort((a, b) => b.lastUpdatedTimestamp - a.lastUpdatedTimestamp);
+      return conversationsArray;
+    } catch (error) {
+      console.error('Error getting conversations:', error);
+      return [];
+    }
+  }
+
+  generateBlurb(conversationId, body = '') {
+    // generateBlurb generates the first 256 characters of last message or if there are typing users, shows who is typing.
+    try {
+      const currentConversation = this.conversations[conversationId];
+      const typingUsers = Array.from(currentConversation.typingUsers);
+
+      if (!typingUsers.length && body) {
+        currentConversation.blurb = body.slice(0, 256);
+      } else if (typingUsers.length === 1) {
+        currentConversation.blurb = `${typingUsers[0]} is replying...`;
+      } else if (typingUsers.length > 1) {
+        currentConversation.blurb = `${typingUsers.join(', ')} are replying...`;
+      } else {
+        currentConversation.blurb = currentConversation.mostRecentMessage.slice(0, 256);
+      }
+    } catch (error) {
+      console.error(`Error generating blurb for conversation ${conversationId}:`, error);
+    }
+  }
+
+  getEventIdentifier(event) {
+    // store event indentifier in processed events to check if there is a duplicate event.
+    const {
+      type,
+      data: { user, timestamp, conversationId, subject },
+    } = event;
+    return `${timestamp}-${conversationId}-${type}-${user || ''}-${subject || ''}`;
+  }
+
+  handleNewConversation(event) {
+    try {
+      const {
+        data: { conversationId, user, subject },
+      } = event;
+
+      this.conversations[conversationId] = {
+        assignedUser: user || null,
+        subject,
+        lastUpdatedTimestamp: 0,
+        messageCount: 0,
+        typingUsers: new Set(),
+      };
+    } catch (error) {
+      console.error('Error handling new conversation:', error);
+    }
+  }
+
+  handleMessageReceived(event) {
+    try {
+      const {
+        data: { conversationId, subject, body },
+      } = event;
+      const currentConversation = this.conversations[conversationId];
+      currentConversation.messageCount++;
+      currentConversation.subject = subject;
+      // keep track of the most recent message to generate blurb in case users stop typing
+      currentConversation.mostRecentMessage = body.slice(0, 256);
+      if (!currentConversation.typingUsers.size) this.generateBlurb(conversationId, body);
+    } catch (error) {
+      console.error('Error handling message received:', error);
+    }
+  }
+
+  handleAssigned(conversation, data) {
+    try {
+      const { user } = data;
+      if (!user) {
+        throw new Error('Invalid user: user cannot be null or undefined in an assigned event.');
+      }
+      conversation.assignedUser = user;
+    } catch (error) {
+      console.error('Error handling assigned event:', error);
+    }
+  }
+
+  handleUnassigned(conversation) {
+    try {
+      conversation.assignedUser = null;
+    } catch (error) {
+      console.error('Error handling unassigned event:', error);
+    }
+  }
+
+  handleTypingStarted(conversation, data) {
+    try {
+      const { user, conversationId } = data;
+      if (!user) {
+        throw new Error('Invalid user: user cannot be null or undefined.');
+      }
+      conversation.typingUsers.add(user);
+
+      // blurb should be updated when there are additional typing users
+      this.generateBlurb(conversationId);
+    } catch (error) {
+      console.error('Error handling typing started event:', error);
+    }
+  }
+
+  handleTypingStopped(conversation, data) {
+    try {
+      const { user, conversationId } = data;
+      conversation.typingUsers.delete(user);
+
+      // update blurb when a user has stopped typing
+      this.generateBlurb(conversationId);
+    } catch (error) {
+      console.error('Error handling typing stopped event:', error);
+    }
+  }
+
   handleEvent(event) {
-    // TODO
-    console.log('Received event', event);
+    try {
+      const { type, data } = event;
+      const { timestamp, conversationId } = data;
+
+      // Throw error if conversationId is null or undefined
+      if (!conversationId) {
+        throw new Error('Invalid conversationId: conversationId cannot be null or undefined.');
+      }
+
+      const eventIdentifier = this.getEventIdentifier(event);
+
+      // Skip duplicate events
+      if (this.processedEvents.has(eventIdentifier)) {
+        return;
+      }
+
+      // if conversation doesn't exist, create a new conversation
+      if (!this.conversations[conversationId]) {
+        this.handleNewConversation(event);
+      }
+
+      let currentConversation = this.conversations[conversationId];
+
+      // update the conversation based on event type
+      switch (type) {
+        case this.eventTypes.MESSAGE_RECEIVED:
+          this.handleMessageReceived(event);
+          break;
+        case this.eventTypes.TYPING_STARTED:
+          this.handleTypingStarted(currentConversation, data);
+          break;
+        case this.eventTypes.TYPING_STOPPED:
+          this.handleTypingStopped(currentConversation, data);
+          break;
+        case this.eventTypes.ASSIGNED:
+          this.handleAssigned(currentConversation, data);
+          break;
+        case this.eventTypes.UNASSIGNED:
+          this.handleUnassigned(currentConversation, data);
+          break;
+        default:
+          // handle unexpected event type
+          console.warn(`Unexpected event type: ${type}`);
+          break;
+      }
+
+      // keep track of processed events in case we receive the same exact event multiple times.
+      this.processedEvents.add(eventIdentifier);
+
+      // update timestamp based on most recent event
+      currentConversation.lastUpdatedTimestamp = Math.max(
+        currentConversation.lastUpdatedTimestamp,
+        timestamp
+      );
+    } catch (error) {
+      console.error('Error handling event:', error);
+    }
   }
 }
---
+++ src/store/store.test.js
@@ -0,0 +1,282 @@
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest';
+import { Store } from './store';
+
+describe('Store', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Store();
+  });
+
+  describe('getConversations', () => {
+    it('should return conversations in reverse chronological order excluding those assigned to John_Doe', () => {
+      store.conversations = {
+        1: { assignedUser: 'John_Doe', lastUpdatedTimestamp: 2 },
+        2: { assignedUser: 'Jason', lastUpdatedTimestamp: 3 },
+        3: { assignedUser: 'Rachel', lastUpdatedTimestamp: 1 },
+      };
+
+      const result = store.getConversations();
+      expect(result).toEqual([
+        { id: '2', assignedUser: 'Jason', lastUpdatedTimestamp: 3 },
+        { id: '3', assignedUser: 'Rachel', lastUpdatedTimestamp: 1 },
+      ]);
+    });
+
+    it('should log error and return empty array when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      store.conversations = null; // This will cause an error
+
+      const result = store.getConversations();
+
+      expect(console.error).toHaveBeenCalledWith('Error getting conversations:', expect.any(Error));
+      expect(result).toEqual([]);
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('generateBlurb', () => {
+    beforeEach(() => {
+      store.conversations = {
+        '1234abcd': {
+          typingUsers: new Set(),
+          blurb: '',
+        },
+      };
+    });
+
+    it('should set the blurb to the first 256 characters of the body when no users are typing', () => {
+      const body =
+        'The project contains a development server serving a front-end client application. The application automatically refreshes when you make changes to the source code. The development server supports both JavaScript and TypeScript.';
+      store.generateBlurb('1234abcd', body);
+      expect(store.conversations['1234abcd'].blurb).toBe(body.slice(0, 256));
+    });
+
+    it('should set the blurb to "[user] is replying..." when one user is typing', () => {
+      store.conversations['1234abcd'].typingUsers.add('Jason');
+      store.generateBlurb('1234abcd');
+      expect(store.conversations['1234abcd'].blurb).toBe('Jason is replying...');
+    });
+
+    it('should set the blurb to "[user1], [user2] are replying..." when two or more users are typing', () => {
+      store.conversations['1234abcd'].typingUsers.add('Jason');
+      store.conversations['1234abcd'].typingUsers.add('Rachel');
+      store.generateBlurb('1234abcd');
+      expect(store.conversations['1234abcd'].blurb).toBe('Jason, Rachel are replying...');
+    });
+
+    it('should log error when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      store.generateBlurb('invalid_id');
+      expect(console.error).toHaveBeenCalledWith(
+        'Error generating blurb for conversation invalid_id:',
+        expect.any(Error)
+      );
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('getEventIdentifier', () => {
+    it('should return a unique identifier for the event', () => {
+      const event = {
+        type: store.eventTypes.MESSAGE_RECEIVED,
+        data: {
+          user: 'Jason',
+          timestamp: 1234567890,
+          conversationId: '1234abcd',
+          subject: 'Hello',
+        },
+      };
+
+      const result = store.getEventIdentifier(event);
+      expect(result).toBe('1234567890-1234abcd-messageReceived-Jason-Hello');
+    });
+  });
+
+  describe('handleNewConversation', () => {
+    it('should add a new conversation', () => {
+      const event = {
+        data: {
+          conversationId: '1234abcd',
+          user: 'Jason',
+          subject: 'Hello',
+        },
+      };
+
+      store.handleNewConversation(event);
+
+      expect(store.conversations['1234abcd']).toEqual({
+        assignedUser: 'Jason',
+        subject: 'Hello',
+        lastUpdatedTimestamp: 0,
+        messageCount: 0,
+        typingUsers: new Set(),
+      });
+    });
+  });
+
+  describe('handleMessageReceived', () => {
+    it('should update the conversation with the new message', () => {
+      store.conversations['1234abcd'] = {
+        assignedUser: 'Jason',
+        subject: 'Hello',
+        lastUpdatedTimestamp: 0,
+        messageCount: 0,
+        typingUsers: new Set(),
+        mostRecentMessage: '',
+      };
+
+      const event = {
+        data: {
+          timestamp: 1234567890,
+          conversationId: '1234abcd',
+          subject: 'Updated Subject',
+          body: 'New message',
+        },
+      };
+
+      store.handleMessageReceived(event);
+
+      expect(store.conversations['1234abcd']).toEqual({
+        assignedUser: 'Jason',
+        subject: 'Updated Subject',
+        lastUpdatedTimestamp: 0,
+        messageCount: 1,
+        typingUsers: new Set(),
+        mostRecentMessage: 'New message',
+        blurb: 'New message',
+      });
+    });
+
+    it('should log error when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      const event = { data: { conversationId: 'invalid_id', body: 'New message' } };
+
+      store.handleMessageReceived(event);
+
+      expect(console.error).toHaveBeenCalledWith('Error handling message received:', expect.any(Error));
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('handleAssigned', () => {
+    it('should assign the user to the conversation', () => {
+      const conversation = {};
+      const data = { user: 'Rachel' };
+
+      store.handleAssigned(conversation, data);
+
+      expect(conversation.assignedUser).toBe('Rachel');
+    });
+
+    it('should log error when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      const conversation = {};
+      const data = { user: null };
+
+      store.handleAssigned(conversation, data);
+
+      expect(console.error).toHaveBeenCalledWith('Error handling assigned event:', expect.any(Error));
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('handleUnassigned', () => {
+    it('should unassign the user from the conversation', () => {
+      const conversation = { assignedUser: 'Rachel' };
+
+      store.handleUnassigned(conversation);
+
+      expect(conversation.assignedUser).toBeNull();
+    });
+
+    it('should log error when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      store.handleUnassigned(null);
+
+      expect(console.error).toHaveBeenCalledWith('Error handling unassigned event:', expect.any(Error));
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('handleTypingStarted', () => {
+    it('should add the user to the typing users set', () => {
+      const conversation = { typingUsers: new Set() };
+      const data = { user: 'Jason', conversationId: '1234abcd' };
+
+      store.handleTypingStarted(conversation, data);
+
+      expect(conversation.typingUsers.has('Jason')).toBe(true);
+    });
+
+    it('should log error when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      store.handleTypingStarted(null, { user: 'Jason', conversationId: '1234abcd' });
+
+      expect(console.error).toHaveBeenCalledWith('Error handling typing started event:', expect.any(Error));
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('handleTypingStopped', () => {
+    it('should remove the user from the typing users set', () => {
+      const conversation = { typingUsers: new Set(['Jason']) };
+      const data = { user: 'Jason', conversationId: '1234abcd' };
+
+      store.handleTypingStopped(conversation, data);
+
+      expect(conversation.typingUsers.has('Jason')).toBe(false);
+    });
+
+    it('should log error when an error occurs', () => {
+      vi.spyOn(console, 'error').mockImplementation(() => {});
+      store.handleTypingStopped(null, { user: 'Jason', conversationId: '1234abcd' });
+
+      expect(console.error).toHaveBeenCalledWith('Error handling typing stopped event:', expect.any(Error));
+      vi.restoreAllMocks();
+    });
+  });
+
+  describe('handleEvent', () => {
+    beforeEach(() => {
+      vi.spyOn(console, 'error').mockImplementation(() => {}); // Mock console.error
+    });
+
+    afterEach(() => {
+      vi.restoreAllMocks(); // Restore console.error mock after each test
+    });
+
+    it('should handle errors gracefully if conversationId is invalid', () => {
+      const event = { type: store.eventTypes.MESSAGE_RECEIVED, data: { conversationId: null } };
+
+      store.handleEvent(event);
+
+      expect(console.error).toHaveBeenCalledWith('Error handling event:', expect.any(Error));
+    });
+
+    it('should process a new MESSAGE_RECEIVED event', () => {
+      const event = {
+        type: store.eventTypes.MESSAGE_RECEIVED,
+        data: {
+          timestamp: 1234567890,
+          conversationId: '1234abcd',
+          user: 'Jason',
+          subject: 'Hello',
+          body: 'Hello, World!',
+        },
+      };
+
+      store.handleEvent(event);
+
+      expect(store.conversations['1234abcd']).toEqual({
+        assignedUser: 'Jason',
+        subject: 'Hello',
+        lastUpdatedTimestamp: 1234567890,
+        messageCount: 1,
+        typingUsers: new Set(),
+        mostRecentMessage: 'Hello, World!',
+        blurb: 'Hello, World!',
+      });
+    });
+  });
+});
